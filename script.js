c9da3d2c06796b7a2c3321164300fa4eb4fc7429
Taylor Miller <trmiller89@gmail.com>
2022-03-18 00:01:24 -0700

    Initial commit, created files/boilerplate

---
+++ script.js
@@ -0,0 +1,11 @@
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+
+  // this.info() {
+  //   console.log(`${title} by ${author}, ${pages} pages, ${read}`);
+  //   return `${title} by ${author}, ${pages} pages, ${read}`;
+  // }
+}
